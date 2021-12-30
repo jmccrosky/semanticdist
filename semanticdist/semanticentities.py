bdce6a9576f378faea528d86627ebbf9626988ec
Jesse McCrosky <mccrosky@gmail.com>
2021-12-30 16:22:17 +0200

    Additional semantic entities code

--- semanticdist/semanticentities.py
+++ semanticdist/semanticentities.py
@@ -1,10 +1,16 @@
 import requests
 
-def get_entities_for_text(text, context):
+def get_single_entities(text, context):
     _params = {
         'text': text,
         'lang': 'AGNOSTIC',
         'annType': 'NAMED_ENTITIES',
         'key': context['babelfy_key']
     }
-    return requests.post("https://babelfy.io/v1/disambiguate", data=_params).json()
+    resp = requests.post("https://babelfy.io/v1/disambiguate", data=_params).json()
+    if "message" in resp:
+        return None
+    return resp
+
+def get_entities(texts, context):
+    return [get_single_entities(text, context) for text in texts]
