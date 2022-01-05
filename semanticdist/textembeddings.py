d111c5afe91f1b4b37fa7678329e36e19b12e9bf
Jesse McCrosky <mccrosky@gmail.com>
2022-01-05 13:29:31 +0200

    Update embedding function to only get needed items

--- semanticdist/textembeddings.py
+++ semanticdist/textembeddings.py
@@ -3,10 +3,15 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 
 def get_embeddings(data, part, context, pickle_file=None):
-    texts = list(data[part])
+    if f'{part}_embedding' in data:
+        needed_indexes = data.index[(~data[part].isnull()) & (
+            data[f'{part}_embedding'].isnull())]
+    else:
+        needed_indexes = data.index[~data[part].isnull()]
+    texts = list(data.loc[needed_indexes, part])
     embeddings = list(context['language_model'].encode(
         texts, show_progress_bar=True))
-    data[f'{part}_embedding'] = embeddings
+    data.loc[needed_indexes, f'{part}_embedding'] = embeddings
     if pickle_file is not None:
         utils.save_data(data, pickle_file, context)
     return data
